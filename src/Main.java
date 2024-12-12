15e8a6fe2ce3dc712031b53fa1bc096b71c63065
TGCAR <don.meln2013@yandex.ru>
2024-12-12 19:16:18 +0300

    HomeWork7PublicStatic

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,38 @@
 public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Task№1");
+        enter();
+        int year = 2000;
+        System.out.println(calculateLeapYear(year));
+        printSeparator();
+        enter();
+
+        System.out.println("Task№2");
+        enter();
+        int os = 0;
+        int currentYear = 2014;
+//        int currentYear = LocalDate.now().getYear();
+        System.out.println(clientDeviceYear(os, currentYear));
+        printSeparator();
+        enter();
+
+        System.out.println("Task№3");
+        enter();
+
+        int days = deliveryDistance(90);
+        if (days == 0) {
+            System.out.println("Доставки нет");
+        } else {
+            System.out.println("Потребуется: " + days + (days == 1 ? " день" : " дня"));
+            printSeparator();
+        }
+    }
+
     public static String calculateLeapYear(int year) {
-        if (year < 1584) {
-            System.out.println(" Год должен быть больше чем 1584. ");
-        } else if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+        if (year < 1584 || (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)) {
             return year + " год является високосным";
         } else return year + " год не является високосным";
-        return "";
     }
 
     public static void enter() {
@@ -31,40 +58,15 @@ public class Main {
         return "";
     }
 
-    public static String deliveryDistance(int km) {
+    public static int deliveryDistance(int km) {
         if (km < 20) {
-            return ("Потребуется дней: " + 1);
+            return 1;
         } else if (km > 20 && km < 60) {
-            return ("Потребуется дней: " + 2);
-        } else if (km > 60 && km < 100) {
-            return ("Потребуется дней: " + 3);
-        } else System.out.println("Доставки нет");
-
-        return "";
-    }
-
-    public static void main(String[] args) {
-
-        System.out.println("Task№1");
-        enter();
-        int year = 1500;
-        System.out.println(calculateLeapYear(year));
-        printSeparator();
-        enter();
-
-        System.out.println("Task№2");
-        enter();
-        int os = 0;
-        int currentYear = 2014;
-//        int currentYear = LocalDate.now().getYear();
-        System.out.println(clientDeviceYear(os, currentYear));
-        printSeparator();
-        enter();
-
-        System.out.println("Task№3");
-        enter();
-        int km = 90;
-        System.out.println(deliveryDistance(km));
-        printSeparator();
+            return 2;
+        } else if (km > 60 && km <= 100) {
+            return 3;
+        } else {
+            return 0;
+        }
     }
 }
